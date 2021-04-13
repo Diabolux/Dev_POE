b3d87e75f5f580580a6225c746723abcfbdc3491
Diabolux <58630829+Diabolux@users.noreply.github.com>
2021-04-13 13:48:03 +0200

    Exercice 1

--- script.js
+++ script.js
@@ -1,57 +1,11 @@
-// toto();
-// function toto(){
-//     console.log("toto");
-// }
+//Exercie catch error
 
-// var machin = function(num1, num2){
-//     sum  = num1 + num2;
-//     return sum;
-// }
-
-// console.log(machin(3, 4));
-
-//Exemple d'erreur, il n'est pas possible de déclarer une const et une fonction avec le même nom ou 2 variables avec le même nom
-// function f(){};
-// const f = 5;
-// function f(){
-//     const g = 6;
-//     var g;
-// }
-
-
-// var division = function(var1, var2){
-//     resultat = var1/var2;
-//     return resultat;
-// }
-//  console.log(division(10,0))
-
-// function carre(b) {
-//     return b*b;
-// }
-// // Afficher la sortie
-// console.log(carre(2));
-// console.log(carre(4));
-
-// function check(numb1, numb2){
-//     return numb1 === numb2;
-// }
-// console.log(check("20", '20'));
-// var tempTab = [];
-// function reverseTab (tab){
-    
-//     for(i = tab.length-1 ; i >= 0  ; i--)
-//     {
-//         tempTab[i] = tab[i];
-//         return tempTab[];
-//     }
-// }
-// var tab = [1,2,5,4,8];
-// reverseTab(tab)
-// console.log(tempTab[]);
-
-var tab = [1,2,3,4,5];
-var i = 0;
-while(i<tab.length){
-    console.log(tab[i]);
-    i++;
+var num1, num2;
+function division(num1, num2) {
+    try {
+        return num1 / num2;
+    } catch (error) {
+        console.log('Impossible de faire une division par 0');
+    }
 }
+console.log(division(4, 'a'));
